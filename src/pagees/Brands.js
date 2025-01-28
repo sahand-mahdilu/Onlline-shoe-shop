652e71933c8228ab3bd87542c709799ec50c1c66
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-01-28 18:48:02 +0330

    feat : brads page created

---
+++ src/pagees/Brands.js
@@ -0,0 +1,14 @@
+import { App } from "../main";
+
+function brands(match){
+    let productBrand= match.data.id
+    console.log(productBrand);
+
+    App.innerHTML=`
+
+    `
+
+}   
+
+
+export{brands}
