4fc08d52b01cf4010edbb26dad774594824b5a0f
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-01-29 19:50:28 +0330

    feat : getting and showing product added

--- src/pagees/SingleProduct.js
+++ src/pagees/SingleProduct.js
@@ -1,29 +1,37 @@
+import { baseURL } from "../API/API";
 import { App } from "../main";
 
-function singleProduct(){
+async function singleProduct(match){
+
+    let productId=match.data.id
+    console.log(productId);
+ let  item= await getData(productId)//array
+ let product=item[0]
+ console.log(product);
+
 
     App.innerHTML=`
         
       <div class="single_product_container ">
-        <div class="img-container relative h-[220px]">
-          <img class="w-full h-full object-cover" src="./shoeimage/imges/nike/nike-10.jpg" alt="img">
+        <div class="img-container max-w-[340px]   relative h-[220px]">
+          <img class="w-full h-full " src=.${product.images} alt="img">
           <svg id="backIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 absolute top-1 left-1">
             <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
           </svg>
         </div>
         <div>
           <div class="flex justify-between px-2">
-            <p class="text-lg font-Sahand">name</p>
+            <p class="text-lg font-Sahand">${product.name}</p>
             <div class="img_container size-6">
-              <img class="w-full h-full object-cover" src="./shoeimage/imges/like.png" alt="like">
+              <img class="w-full h-full object-cover" src="../shoeimage/imges/like.png" alt="like">
             </div>
           </div>
           <div class="px-2 flex items-center gap-3 mt-2">
-            <div class="p-1 bg-slate-200 w-max px-2 rounded-md text-[12px] font-semibold">5845 sold</div>
+            <div class="p-1 bg-slate-200 w-max px-2 rounded-md text-[12px] font-semibold">${product.sold} sold</div>
             <div class="img_container size-5">
-              <img class="w-full h-full object-cover" src="./public/shoeimage/imges/star.png" alt="">
+              <img class="w-full h-full object-cover" src="../public/shoeimage/imges/star.png" alt="">
             </div>
-            <span class="rate text-[13px]">4.3 (5.359 reviews)</span>
+            <span class="rate text-[13px]">${product.rate} (5.359 reviews)</span>
           </div>
           <div class="px-2 mt-5">
             <p class="text-lg font-semibold">Description</p>
@@ -33,32 +41,32 @@ function singleProduct(){
             <div>
               <span class=" font-semibold text-[17px]">size</span>
             <div class="size flex gap-1">
-              <div class="flex items-center justify-center size-6 border border-black rounded-full text-[12px] text-white bg-black ">35</div>
-              <div class="flex items-center justify-center size-6 border border-black rounded-full text-[12px] ">40</div>
-              <div class="flex items-center justify-center size-6 border border-black rounded-full text-[12px] ">42</div>
+              <div class="flex items-center justify-center size-6 border border-black rounded-full text-[12px] text-white bg-black ">${product.sizes[0]}</div>
+              <div class="flex items-center justify-center size-6 border border-black rounded-full text-[12px] ">${product.sizes[1]}</div>
+              <div class="flex items-center justify-center size-6 border border-black rounded-full text-[12px] ">${product.sizes[2]}</div>
             </div>
           </div>
 
             <div>
               <span class="font-semibold text-[17px]">color</span>
               <div class="colors flex gap-1">
-                <div class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+                <div style=background-color:${product.colors[0]} class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-white">
                   <path fill-rule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
                 </svg>
                 </div>
-                <div class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+                <div style=background-color:${product.colors[1]}  class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-white">
                   <path fill-rule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
                 </svg>
                 </div>
-                <div class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+                <div style=background-color:${product.colors[2]} class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-white">
                   <path fill-rule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
                 </svg>
                 </div>
-                <div class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+                <div style=background-color:${product.colors[3]} class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-white">
                   <path fill-rule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
                 </svg>
                 </div>
-                <div class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+                <div style=background-color:${product.colors[4]}  class="flex items-center justify-center size-6 border border-black rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-white">
                   <path fill-rule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
                 </svg>
                 </div>
@@ -75,7 +83,7 @@ function singleProduct(){
             <span class="text-lg font-semibold">Quantity</span>
             <div class="bg-slate-200 w-20 flex justify-between items-center  px-2 rounded-md text-[15px]">
               <button class="text-[18px] font-semibold">-</button>
-              <span class="quantity text-[14px]">100</span>
+              <span class="quantity text-[14px]">1</span>
               <button class="text-[18px] font-semibold">+</button>
             </div>
           </div>
@@ -88,7 +96,7 @@ function singleProduct(){
               <div>
                 <button class="flex items-center justify-center   bg-black text-white p-[6px] min-w-48 rounded-2xl text-[14px]">
                  <div class="flex items-center gap-1 mr-2">
-                  <img  src="./public/shoeimage/imges/carticon.png" alt=""> <span>
+                  <img  src="../public/shoeimage/imges/carticon.png" alt=""> <span>
                     Add to cart
                   </span>
                  </div> </button>
@@ -103,6 +111,18 @@ function singleProduct(){
       </div>
 
 `
+}
+
+async function getData(id){
+
+    try{
+        let res =  await fetch(`${baseURL}/products?id=${id}`)
+        let  data=await res.json()
+        console.log(data);
+        return data
+    }catch(err){
+        console.log(err);
+    }
 }
 
 
