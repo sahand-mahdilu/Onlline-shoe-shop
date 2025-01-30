99aee79d27ea622ad0b15f5ea703bd484dfda89d
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-01-30 17:56:38 +0330

    feat : bin icon changed

--- src/pagees/Cart.js
+++ src/pagees/Cart.js
@@ -37,38 +37,7 @@ function cart(){
         </div>
         <div class="product_container mb-16">
 
-          <div class="product_cart flex items-center mt-5 rounded-3xl bg-green-100">
-            <div class="img_container w-[150px] rounded-3xl overflow-hidden">
-              <img class="w-full h-full" src="./public/shoeimage/imges/nike/nike-10.jpg" alt="">
-            </div>
-            <div class="w-full p-2">
-              <div class="flex items-center justify-between px-2">
-                <p class="text-lg font-bold">name</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                </svg>
-                
-
-              </div>
-
-              <div class="flex gap-2 mt-4">
-                <div class="color size-6 bg-orange-400 rounded-full"></div>
-                <span class="color text-[13px] text-gray-500">orange</span>
-                <div class="h-4 border w-0 border-gray-300"></div>
-                <span class="text-gray-500 text-[13px]">size = 42</span>
-              </div>
-              <div class="flex items-center justify-between mt-4 px-2">
-                <span class="totalPrice font-bold">
-                  $150
-                </span>
-                <div class="bg-gray-200 w-16 rounded-md px-2 flex items-center justify-between ">
-                  <button>-</button>
-                  <span class="Quantity">1</span>
-                  <button>+</button>
-                </div>
-              </div>
-            </div>
-          </div>
+          
 
 
 
@@ -127,6 +96,8 @@ function cart(){
     const cartBtn=document.querySelector("#cart")
     const homeBtn=document.querySelector("#home")
     const productContainerElem= document.querySelector(".product_container")
+    
+  
 
     
 
@@ -136,7 +107,6 @@ function cart(){
 }
 
 function activeButton (cartElem,homeElem){
-    console.log(homeElem);
 
     cartElem.src="./shoeimage/imges/cart-2.png"
     homeElem.src="./shoeimage/imges/white-home.png"
@@ -150,7 +120,6 @@ async function getCartData(container){
             method:"GET"
         })
         let data= await res.json()//array
-        console.log(data);
         showProduct(data,container)
     }catch(err){
         console.log(err);
@@ -159,9 +128,9 @@ async function getCartData(container){
 
 }
 
-function showProduct(productArray,container){
+ function showProduct(productArray,container){
 
-    productArray.forEach((product)=>{
+     productArray.forEach((product)=>{
         container.insertAdjacentHTML("afterbegin",`<div class="product_cart flex items-center mt-5 rounded-3xl bg-green-100">
             <div class="img_container w-[150px] rounded-3xl overflow-hidden">
               <img class="w-full h-full" src=${product.image} alt="">
@@ -169,9 +138,9 @@ function showProduct(productArray,container){
             <div class="w-full p-2">
               <div class="flex items-center justify-between px-2">
                 <p class="text-lg font-bold">${product.name}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                </svg>
+                <img id=${product.id} class="trash size-10" src="./public/shoeimage/imges/recycle.webp">
+
+                
                 
 
               </div>
@@ -195,7 +164,34 @@ function showProduct(productArray,container){
             </div>
           </div>`)
 
+        
+
     })
+
+    const trashbtn= document.querySelectorAll(".trash")
+    console.log(trashbtn);
+    deleteProduct(trashbtn)
+}
+
+ function deleteProduct(deleteButtons){
+
+    for(let button of deleteButtons){
+
+         button.addEventListener("click",function(e){
+            let id=e.target.id
+            console.log(id);
+            
+        })
+    }
+
+   
+
+    // let res= await fetch(`${baseURL}/cart?id=${id}`,{
+    //     method:"DELETE"
+    // })
+    // let data= await res.json()
+    // console.log(data);
+
 }
 
 
