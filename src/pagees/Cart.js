5867c103ba09153378accad67361b80de58df655
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-01-30 17:24:52 +0330

    feat: get data form backend added

--- src/pagees/Cart.js
+++ src/pagees/Cart.js
@@ -1,3 +1,4 @@
+import { baseURL } from "../API/API";
 import { App } from "../main";
 import { navigationBar } from "../NavigationBar/NavigationBar";
 import { navBar } from "./Home";
@@ -125,11 +126,13 @@ function cart(){
     const navButtons = document.querySelectorAll(".navigation");
     const cartBtn=document.querySelector("#cart")
     const homeBtn=document.querySelector("#home")
+    const productContainerElem= document.querySelector(".product_container")
 
-
+    
 
     navBar(navButtons)
     activeButton(cartBtn,homeBtn)
+    getCartData(productContainerElem)
 }
 
 function activeButton (cartElem,homeElem){
@@ -137,6 +140,62 @@ function activeButton (cartElem,homeElem){
 
     cartElem.src="./shoeimage/imges/cart-2.png"
     homeElem.src="./shoeimage/imges/white-home.png"
+}
+
+
+async function getCartData(container){
+    try{
+
+        let res = await fetch(`${baseURL}/cart`,{
+            method:"GET"
+        })
+        let data= await res.json()//array
+        console.log(data);
+        showProduct(data,container)
+    }catch(err){
+        console.log(err);
+    }
+    
+
+}
+
+function showProduct(productArray,container){
+
+    productArray.forEach((product)=>{
+        container.insertAdjacentHTML("afterbegin",`<div class="product_cart flex items-center mt-5 rounded-3xl bg-green-100">
+            <div class="img_container w-[150px] rounded-3xl overflow-hidden">
+              <img class="w-full h-full" src=${product.image} alt="">
+            </div>
+            <div class="w-full p-2">
+              <div class="flex items-center justify-between px-2">
+                <p class="text-lg font-bold">${product.name}</p>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+                
+
+              </div>
+
+              <div class="flex gap-2 mt-4">
+                <div style= background-color:${product.color} ; class="color size-6  rounded-full"></div>
+                <span class="color text-[13px] text-gray-500">${product.color}</span>
+                <div class="h-4 border w-0 border-gray-300"></div>
+                <span class="text-gray-500 text-[13px]">size = ${product.size}</span>
+              </div>
+              <div class="flex items-center justify-between mt-4 px-2">
+                <span class="totalPrice font-bold">
+                  $${product.price}
+                </span>
+                <div class="bg-gray-200 w-16 rounded-md px-2 flex items-center justify-between ">
+                  <button>-</button>
+                  <span class="Quantity">${product.count}</span>
+                  <button>+</button>
+                </div>
+              </div>
+            </div>
+          </div>`)
+
+    })
 }
 
 
