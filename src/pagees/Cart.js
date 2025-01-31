035351368fe40c81d12e26c7c2bcc5fade441af4
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-01-31 15:39:48 +0330

    feat : deleting data added

--- src/pagees/Cart.js
+++ src/pagees/Cart.js
@@ -3,9 +3,8 @@ import { App } from "../main";
 import { navigationBar } from "../NavigationBar/NavigationBar";
 import { navBar } from "./Home";
 
-
-function cart(){
-    App.innerHTML=`
+function cart() {
+  App.innerHTML = `
   
           
       <div class="cart_container p-3">
@@ -35,7 +34,7 @@ function cart(){
             </svg>
           </div>
         </div>
-        <div class="product_container mb-16">
+        <div class="product_container mb-24">
 
           
 
@@ -91,70 +90,63 @@ function cart(){
    ${navigationBar()}
 
 
-    `
-    const navButtons = document.querySelectorAll(".navigation");
-    const cartBtn=document.querySelector("#cart")
-    const homeBtn=document.querySelector("#home")
-    const productContainerElem= document.querySelector(".product_container")
-    
-  
+    `;
+  const navButtons = document.querySelectorAll(".navigation");
+  const cartBtn = document.querySelector("#cart");
+  const homeBtn = document.querySelector("#home");
+  const productContainerElem = document.querySelector(".product_container");
 
-    
-
-    navBar(navButtons)
-    activeButton(cartBtn,homeBtn)
-    getCartData(productContainerElem)
+  navBar(navButtons);
+  activeButton(cartBtn, homeBtn);
+  getCartData(productContainerElem);
 }
 
-function activeButton (cartElem,homeElem){
-
-    cartElem.src="./shoeimage/imges/cart-2.png"
-    homeElem.src="./shoeimage/imges/white-home.png"
+function activeButton(cartElem, homeElem) {
+  cartElem.src = "./shoeimage/imges/cart-2.png";
+  homeElem.src = "./shoeimage/imges/white-home.png";
 }
 
+async function getCartData(container) {
+  try {
+    let res = await fetch(`${baseURL}/cart`, {
+      method: "GET",
+    });
+    let data = await res.json(); //array
 
-async function getCartData(container){
-    try{
+    if (data.length === 0) {
+      let totalElem = document.querySelector(".totalProductPrice");
+      totalElem.innerHTML = `$0`;
 
-        let res = await fetch(`${baseURL}/cart`,{
-            method:"GET"
-        })
-        let data= await res.json()//array
-        console.log("sahand");
-
-        if(data.length===0){
-             let totalElem = document.querySelector(".totalProductPrice")
-        totalElem.innerHTML=`$0`
-
- container.insertAdjacentHTML("afterbegin",`
+      container.insertAdjacentHTML(
+        "afterbegin",
+        `
     <div class="mt-3 max-w-[350px] mx-auto h-[300px]">
       <img class="w-full h-full" src="./public/shoeimage/imges/notfound.png" alt="">
     
     </div>
     <P class="mt-2 text-center text-xl font-Sahand">Cart is emptey ☹️</P>
-    `)
-        }else{
-
-            showProduct(data,container)
-        }
-    }catch(err){
-        console.log(err);
+    `
+      );
+    } else {
+      showProduct(data, container);
     }
-    
-
+  } catch (err) {
+    console.log(err);
+  }
 }
 
- function showProduct(productArray,container){
-
-     productArray.forEach((product)=>{
-        container.insertAdjacentHTML("afterbegin",`<div class="product_cart flex items-center mt-5 rounded-3xl bg-green-100">
+function showProduct(productArray, container) {
+  productArray.forEach((product) => {
+    container.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="product_cart  flex items-center mt-5 rounded-3xl bg-green-100">
             <div class="img_container w-[150px] rounded-3xl overflow-hidden">
               <img class="w-full h-full" src=${product.image} alt="">
             </div>
             <div class="w-full p-2">
               <div class="flex items-center justify-between px-2">
                 <p class="text-lg font-bold">${product.name}</p>
-                <img id=${product.id} class="trash size-10" src="./public/shoeimage/imges/recycle.webp">
+                <img id=${product.id}   data-product-name=${product.name} data-product-color=${product.color} data-product-size=${product.size} data-product-image=${product.image}   class="trash size-10" src="./public/shoeimage/imges/recycle.webp">
 
                 
                 
@@ -193,12 +185,12 @@ async function getCartData(container){
             <div class="p-2">
               <div class="product_cart flex items-center mt-5 rounded-3xl bg-green-100">
                 <div class="img_container w-[150px] rounded-3xl overflow-hidden">
-                  <img class="w-full h-full" src=${product.image} alt="">
+                  <img class="p_img w-full h-full" src=${product.image} alt="">
                 </div>
                 <div class="w-full p-2">
                   <div class="flex items-center justify-between px-2">
-                    <p class="text-lg font-bold">${product.name}</p>
-                    <img id=${product.id} class="trash size-10" src="./public/shoeimage/imges/recycle.webp">
+                    <p class="p_name text-lg font-bold">${product.name}</p>
+                    <img id=${product.id} class=" size-10" src="./public/shoeimage/imges/recycle.webp">
     
                     
                     
@@ -206,20 +198,14 @@ async function getCartData(container){
                   </div>
     
                   <div class="flex gap-2 mt-4">
-                    <div style= background-color:${product.color} ; class="color size-6  rounded-full"></div>
-                    <span class="color text-[13px] text-gray-500">${product.color}</span>
+                    <div style= background-color:${product.color} ; class="color p_color size-6  rounded-full"></div>
+                    <span class="c_text color text-[13px] text-gray-500">${product.color}</span>
                     <div class="h-4 border w-0 border-gray-300"></div>
-                    <span class="text-gray-500 text-[13px]">size = ${product.size}</span>
+                    <span class="p-size text-gray-500 text-[13px]">size = ${product.size}</span>
                   </div>
                   <div class="priceSection flex items-center justify-between mt-4 px-2">
-                    <span class="productPrice font-bold data-base-price=${product.price}">
-                      $${product.price}
-                    </span>
-                    <div class="bg-gray-200 w-16 rounded-md px-2 flex items-center justify-between ">
-                      <button class="minus" >-</button>
-                      <span class="Quantity">${product.count}</span>
-                      <button class="plus" >+</button>
-                    </div>
+                   
+                   
                   </div>
                 </div>
               </div>
@@ -228,8 +214,8 @@ async function getCartData(container){
 
               <div class="h-px  border-[1px] border-gray-300 mb-2 mt-4"></div>
               <div class="flex justify-center gap-4 py-4">
-                <button class="p-[6px] w-28 rounded-2xl bg-gray-400">Cancel</button>
-                <button class="p-[6px] w-28 rounded-2xl bg-black text-white">Yes,Remove</button>
+                <button class="cancel p-[6px] w-28 rounded-2xl bg-gray-400">Cancel</button>
+                <button class=" remove_p p-[6px] w-28 rounded-2xl bg-black text-white">Yes,Remove</button>
               </div>
             </div>
           </div>
@@ -239,184 +225,189 @@ async function getCartData(container){
        </div>
 
           
-          `)
+          `
+    );
+  });
 
-        
+  const trashbtn = document.querySelectorAll(".trash");
+  const plusBtn = document.querySelectorAll(".plus");
+  const minusBtn = document.querySelectorAll(".minus");
+  const productPrice = document.querySelectorAll(".productPrice");
+  const quantityElemm = document.querySelectorAll(".Quantity");
 
-    })
-
-    const trashbtn= document.querySelectorAll(".trash")
-    const plusBtn=document.querySelectorAll(".plus")
-    const minusBtn=document.querySelectorAll(".minus")
-    const productPrice=document.querySelectorAll(".productPrice")
-    const quantityElemm=document.querySelectorAll(".Quantity")
-
-    
-    removeProduct(trashbtn,container)
-    PriceCalculater(plusBtn,minusBtn,quantityElemm,productPrice)
+  PriceCalculater(plusBtn, minusBtn, quantityElemm, productPrice);
+  removeProduct(trashbtn, container, );
 }
 
-  function removeProduct(deleteButtons,container){
-    let modalContainer= document.querySelector(".modal_container")
-    let modalElemr= document.querySelector(".modal")
+function removeProduct(deleteButtons, container,) {
 
 
+  const modalContainer = document.querySelector(".modal_container");
+  const modalElemr = document.querySelector(".modal");
+  const cancelButton = document.querySelector(".cancel");
+  const removeButton = document.querySelector(".remove_p");
+  let id;
 
+  cancelButton.addEventListener("click", function () {
+    modalContainer.classList.remove("opacity-100", "visible");
+    modalContainer.classList.add("opacity-0", "invisible");
 
-    for(let delButton of deleteButtons){
+    modalElemr.classList.remove("bottom-0");
+    modalElemr.classList.add("bottom-[-100%]");
+  });
 
-      delButton.addEventListener("click",function(){
+  for (let delButton of deleteButtons) {
+    delButton.addEventListener("click", function (e) {
 
-        modalContainer.classList.remove("opacity-0" ,"invisible")
-        modalContainer.classList.add("opacity-100","visible")
-        modalElemr.classList.remove("bottom-[-100%]")
-        modalElemr.classList.add("bottom-0")
+      id=e.target.id
+      deleteProduct(id,removeButton,container)
 
-      })
-    }
+      let productName=e.target.dataset.productName
+      let productImage=e.target.dataset.productImage
+      let productSize=e.target.dataset.productSize
+      let productColor=e.target.dataset.productColor
+   
+     
 
-
-    // for(let button of deleteButtons){
-
-    //      button.addEventListener("click",async function(e){
-    //         let id=e.target.id
-    //         console.log(id);
-                
-    //         try{
-    //             let res = await fetch(`${baseURL}/cart/${id}`,{
-    //                 method:"DELETE"
-    //             })
-                
-                
-    //             console.log(res);
-    //             if(res.ok){
-    //                 console.log("ok");
-    //                  container.innerHTML = ''
-
-    //                 getCartData(container)
-    //             }
-    //         }catch(err){
-    //             console.log(err);
-    //         }
-            
-    //     })
-    // }
+    
+      document.querySelector(".p_img").src=productImage
+      document.querySelector(".p_name").textContent=productName
+      document.querySelector(".p_color").style.backgroundColor=productColor
+      document.querySelector(".c_text").innerHTML=productColor
+      document.querySelector(".p-size").innerHTML=`size=${productSize}`
+     
 
    
 
+
+
+
+      modalContainer.classList.remove("opacity-0", "invisible");
+      modalContainer.classList.add("opacity-100", "visible");
+      modalElemr.classList.remove("bottom-[-100%]");
+      modalElemr.classList.add("bottom-0");
+    });
+  }
+
+
+ 
+
+ 
+
+    /////////////////////////////////////////////////////////////////
+   
+
+      
+
+
+  }
+
+
+  function deleteProduct(id,delButton,container){
+
+    delButton.addEventListener("click",async function(e){
+     
+      
+
+      try{
+          let res = await fetch(`${baseURL}/cart/${id}`,{
+              method:"DELETE"
+          })
+
+          console.log(res);
+          if(res.ok){
+              console.log("ok");
+               container.innerHTML = ''
+
+              getCartData(container)
+          }
+      }catch(err){
+          console.log(err);
+      }
+
+  })
+
+
+  }
+
+function PriceCalculater(plusbtns, minusbtns, quantities, prices) {
+
+  const trash= document.querySelector(".trash")
+
+  for (let plusButton of plusbtns) {
+    plusButton.addEventListener("click", function () {
+      let parent = plusButton.closest(".priceSection");
+
+      let priceElem = parent.querySelector(".productPrice");
+
+      let price = Number(priceElem.textContent.trim().slice(1));
+      let quantity = Number(plusButton.previousElementSibling.textContent);
+
+      let basePrice = price / quantity;
+
+      let sum = price;
+      plusButton.previousElementSibling.textContent++;
     
-
-}
-
-
-function PriceCalculater(plusbtns,minusbtns,quantities,prices){
-    console.log(prices);
-
-    for(let plusButton of plusbtns){
-
-
-           
-        plusButton.addEventListener("click",function(){
-            let parent= plusButton.closest(".priceSection")
-
-            let priceElem = parent.querySelector(".productPrice")
-    
-            let price =  Number(priceElem.textContent.trim().slice(1))
-            let quantity = Number(plusButton.previousElementSibling.textContent)
-            
-            let basePrice=price/quantity
-                
-         
-    
-    
-            let sum = price     
-            plusButton.previousElementSibling.textContent++
-
-            sum+=basePrice
-            console.log(sum);
-            priceElem.textContent=`$${sum}` 
-
-
-
-            totalPriceHandler(prices)
-        
-        })
+     
 
 
       
-    }
-    for(let minusButton of minusbtns){
-        
+      sum += basePrice;
+      priceElem.textContent = `$${sum}`;
+   
+
+      ///////////////////////////////////////////////////////////////////
+
+      totalPriceHandler(prices);
+    });
+  }
+  for (let minusButton of minusbtns) {
+    minusButton.addEventListener("click", function () {
+      let parent = minusButton.closest(".priceSection");
+
+      let priceElem = parent.querySelector(".productPrice");
+
+      let price = Number(priceElem.textContent.trim().slice(1));
+      let quantity = Number(minusButton.nextElementSibling.textContent);
+
+      let basePrice = price / quantity;
+
+      console.log(price);
+
+      let sum = price;
+
+      let content = minusButton.nextElementSibling.textContent;
+      console.log(content);
+      if (content > 1) {
+        minusButton.nextElementSibling.textContent--;
+      
+        sum -= basePrice;
+        console.log(sum);
+        priceElem.textContent = `$${sum}`;
        
+      }
 
-        minusButton.addEventListener("click",function(){
-            let parent= minusButton.closest(".priceSection")
+      totalPriceHandler(prices);
+    });
+  }
+  totalPriceHandler(prices);
+}
 
-            let priceElem = parent.querySelector(".productPrice")
-    
-            let price =  Number(priceElem.textContent.trim().slice(1))
-            let quantity = Number(minusButton.nextElementSibling.textContent)
-            
-            let basePrice=price/quantity
-    
-            console.log(price);
-                
-         
-    
-    
-            let sum = price  
+function totalPriceHandler(prices) {
+  let priceArray = [];
+  for (let price of prices) {
+    let productprice = Number(price.textContent.trim().slice(1));
+    priceArray.push(productprice);
+  }
 
-            let content= minusButton.nextElementSibling.textContent
-            console.log(content);
-            if(content>1){
-                minusButton.nextElementSibling.textContent--
-                sum-=basePrice
-                console.log(sum);
-                priceElem.textContent=`$${sum}` 
-            }
+  let totalElem = document.querySelector(".totalProductPrice");
 
-            totalPriceHandler(prices)
-        })
-    }
-    totalPriceHandler(prices)
+  let totalPrice = priceArray.reduce((acc, current) => {
+    return acc + current;
+  }, 0);
+
+  totalElem.innerHTML = `$${totalPrice}`;
 
 }
 
-function totalPriceHandler(prices){
-
-   console.log();
-    console.log(prices);
-
-   
-
-        let priceArray=[]
-        for(let price of prices){
-    
-            let productprice=Number(price.textContent.trim().slice(1))
-            priceArray.push(productprice)
-    
-            
-            
-        }
-    
-        let totalElem = document.querySelector(".totalProductPrice")
-        
-        
-        let totalPrice= priceArray.reduce((acc,current)=>{
-            return acc+current
-        },0)
-        
-        totalElem.innerHTML=`$${totalPrice}`
-        
-        console.log(totalPrice)
-   
-        
-    
-   
-
-}
-
-
-
-
-export{cart}
+export { cart };
