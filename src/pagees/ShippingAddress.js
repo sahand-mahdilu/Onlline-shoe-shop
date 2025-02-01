d054498b695c3c5e90f339087bb62b3b9f6d04b7
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-01 15:02:56 +0330

    feat : navigation added

--- src/pagees/ShippingAddress.js
+++ src/pagees/ShippingAddress.js
@@ -1,14 +1,14 @@
-import { App } from "../main";
+import { App, router } from "../main";
 
-function shippingAddress(){
-    App.innerHTML=`
+function shippingAddress() {
+  App.innerHTML = `
        <div class="shipping_address_container p-2">
 
 
         <form class="" action="">
 
           <div class="flex gap-3 pt-1 items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 goBack">
               <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
             </svg>
             <h2 class="text-xl font-Sahand">Shipping Address</h2>
@@ -122,20 +122,21 @@ function shippingAddress(){
 
 
       </div>
-
-
-
-
-      
-
-
-      
-
     
     `
+    
+    goBack()
 }
 
 
+function goBack(){
+    const goBackButton= document.querySelector(".goBack")
 
+    goBackButton.addEventListener("click",function(){
 
-export{shippingAddress}
+        router.navigate("/checkout")
+    })
+
+}
+
+export { shippingAddress };
