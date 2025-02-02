39110033fb882c08f1d8d3f1410ff55c7b73c7b3
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-02 21:52:11 +0330

    feat : get completed data added

--- src/pagees/Order.js
+++ src/pagees/Order.js
@@ -59,7 +59,7 @@ function order(){
         </div>
 
 
-        <div class="product_container">
+        <div class="product_container mb-16">
           
         </div>
 
@@ -105,6 +105,7 @@ function activeButton(orderElem, homeElem) {
 
         let data = await res.json()//array
         console.log(data);
+        showData(data)
 
     }catch(err){
         console.log(err);
@@ -113,6 +114,90 @@ function activeButton(orderElem, homeElem) {
 
 
   }
+
+
+  
+  async function getCompleteData(){
+
+    try{
+
+        let res = await fetch(`${baseURL}/completed`,{
+            method:"GET"
+        })
+
+        let data = await res.json()//array
+        console.log(data);
+        showData(data)
+
+    }catch(err){
+        console.log(err);
+    }
+
+
+
+  }
+
+
+  function showData(products){
+
+    const productContainer= document.querySelector(".product_container")
+    console.log(productContainer);
+
+    products.forEach(product=>{
+
+        productContainer.insertAdjacentHTML("afterbegin",`<div class="cart product_cart  flex items-center mt-5 rounded-3xl bg-green-100">
+      <div class="img_container w-[150px] rounded-3xl overflow-hidden">
+      <img class="image w-full h-full" src=${product.image} alt="img">
+      </div>
+      <div class="w-full p-2">
+      <div class="flex items-center justify-between px-2">
+      <p class="name text-lg font-bold">${product.name}</p>
+      
+      
+      
+      
+      
+      </div>
+      
+      <div class="flex gap-2 mt-4">
+      <div style= "background-color:${product.color} " class="color size-6  rounded-full"></div>
+      <span class="color text-[13px] text-gray-500">${product.color}</span>
+      <div class="h-4 border w-0 border-gray-300"></div>
+      <span class="size text-gray-500 text-[13px]">size=${product.size}</span>
+      <span class="status text-[13px] bg-black text-white  rounded-xl px-2 ">active</span>
+      </div>
+      <div class="priceSection flex items-center justify-between mt-4 px-2">
+      <span class="price productPrice font-bold ">
+      $${product.price}
+      </span>
+      <div class="bg-gray-200 rounded-full size-8 font-semibold  px-2 flex items-center justify-center ">
+      ${product.count}
+      </div>
+      </div>
+      </div>
+      </div>
+      `)
+
+
+      
+      
+      const status= document.querySelector(".status")
+      if(!product.status){
+
+        status.textContent="active"
+    }else{
+        
+        status.textContent="completed"
+        
+      }
+      
+    })
+    
+
+
+  }
+
+  
 
 
   
@@ -126,6 +211,7 @@ function activeButton(orderElem, homeElem) {
     const completeBtn =document.querySelector('.completeBtn')
     const activeBar=document.querySelector(".active_bar")
     const completeBar=document.querySelector(".completed_bar")
+    const productContainer= document.querySelector(".product_container")
 
 
     activeBtn.addEventListener("click",function(){
@@ -134,6 +220,10 @@ function activeButton(orderElem, homeElem) {
         activeBar.classList.remove("bg-gray-300")
         completeBar.classList.remove("bg-black")
         completeBar.classList.add("bg-gray-300")
+        productContainer.innerHTML=""
+
+
+        getActiveData()
 
     })
 
@@ -143,6 +233,10 @@ function activeButton(orderElem, homeElem) {
         activeBar.classList.add("bg-gray-300")
         completeBar.classList.add("bg-black")
         completeBar.classList.remove("bg-gray-300")
+        productContainer.innerHTML=""
+
+
+        getCompleteData()
     })
 
    
